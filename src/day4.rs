6bc2a33924f448b45dc690748b0fe1e850c04281
Manuel Müller <manuel.mueller@geekinbusiness.de>
2021-12-05 19:56:45 +0100

    rust formatter

--- src/day4.rs
+++ src/day4.rs
@@ -1,14 +1,16 @@
 //use crate::{utils};
 
-#[derive(Copy, Clone,Debug)]
-struct Cell{
+use crate::utils;
+
+#[derive(Copy, Clone, Debug)]
+struct Cell {
     value: i32,
     marked: bool,
 }
 
 #[derive(Copy, Clone)]
-struct BingoBoard{
-    rows: [[Cell;5];5]
+struct BingoBoard {
+    rows: [[Cell; 5]; 5],
 }
 
 // impl fmt::Display for BingoBoard {
@@ -24,30 +26,31 @@ struct BingoBoard{
 //     }
 // }
 
-
 impl BingoBoard {
-    fn new() -> BingoBoard{
-        return BingoBoard{
-            rows: [[Cell{value:404,marked:false};5];5]
-        }
+    fn new() -> BingoBoard {
+        return BingoBoard {
+            rows: [[Cell {
+                value: 404,
+                marked: false,
+            }; 5]; 5],
+        };
     }
 
     fn is_finished(&self) -> bool {
         //(Diagonals don't count.)
-        return self.check_rows()
-            || self.check_columns();
+        return self.check_rows() || self.check_columns();
     }
 
-    fn check_row(cells: &[Cell;5]) -> bool {
+    fn check_row(cells: &[Cell; 5]) -> bool {
         for cell in cells.iter() {
-            if ! cell.marked {
-                return false
+            if !cell.marked {
+                return false;
             }
         }
         return true;
     }
     fn check_rows(&self) -> bool {
-        for row in self.rows.iter(){
+        for row in self.rows.iter() {
             if BingoBoard::check_row(row) {
                 return true;
             }
@@ -56,9 +59,9 @@ impl BingoBoard {
     }
     fn check_columns(&self) -> bool {
         for i in 0..4 {
-            let mut completed= true;
-            for row in self.rows.iter(){
-                if ! row[i].marked {
+            let mut completed = true;
+            for row in self.rows.iter() {
+                if !row[i].marked {
                     completed = false;
                 }
             }
@@ -69,22 +72,22 @@ impl BingoBoard {
         return false;
     }
 
-    fn mark_value(&mut self, value: i32) -> bool{
-        let mut marked=false;
+    fn mark_value(&mut self, value: i32) -> bool {
+        let mut marked = false;
         for row in self.rows.iter_mut() {
-            for cell in row.iter_mut(){
+            for cell in row.iter_mut() {
                 if cell.value == value {
                     cell.marked = true;
-                    marked=true;
+                    marked = true;
                 }
             }
         }
         return marked;
     }
-    fn board_score(&self) -> i32{
-        let mut score= 0;
+    fn board_score(&self) -> i32 {
+        let mut score = 0;
         for row in self.rows.iter() {
-            for cell in row.iter(){
+            for cell in row.iter() {
                 if !cell.marked {
                     score = score + cell.value
                 }
@@ -94,9 +97,9 @@ impl BingoBoard {
     }
 }
 
-fn read_bingo_numbers (line: &String) -> Vec<i32>{
+fn read_bingo_numbers(line: &String) -> Vec<i32> {
     let mut bingo_numbers = Vec::new();
-    let raw_inputs=line.split(",").collect::<Vec<&str>>();
+    let raw_inputs = line.split(",").collect::<Vec<&str>>();
     for raw in raw_inputs.iter() {
         let z = (raw.to_string()).parse::<i32>().unwrap();
         bingo_numbers.push(z);
@@ -104,9 +107,9 @@ fn read_bingo_numbers (line: &String) -> Vec<i32>{
     return bingo_numbers;
 }
 
-fn read_row_numbers (line: &String) -> Vec<i32>{
+fn read_row_numbers(line: &String) -> Vec<i32> {
     let mut numbers = Vec::new();
-    let raw_inputs=line.split_whitespace().collect::<Vec<&str>>();
+    let raw_inputs = line.split_whitespace().collect::<Vec<&str>>();
     for raw in raw_inputs.iter() {
         let z = (raw.to_string()).parse::<i32>().unwrap();
         numbers.push(z);
@@ -114,73 +117,78 @@ fn read_row_numbers (line: &String) -> Vec<i32>{
     return numbers;
 }
 
-fn setup_boards (lines: Vec<String>) -> Vec<BingoBoard>{
+fn setup_boards(lines: Vec<String>) -> Vec<BingoBoard> {
     let mut boards = Vec::new();
 
     let mut board = BingoBoard::new();
-    let mut row_number=0;
+    let mut row_number = 0;
     for i in 1..lines.len() {
-        let line=lines.get(i).unwrap();
-        if line.is_empty(){
+        let line = lines.get(i).unwrap();
+        if line.is_empty() {
             // simly accept that one board is unusable
             boards.push(board);
-            board=BingoBoard::new();
-            row_number=0;
-        } else{
+            board = BingoBoard::new();
+            row_number = 0;
+        } else {
             let numbers = read_row_numbers(line);
-            assert_eq!(numbers.len(),board.rows[row_number].len());
+            assert_eq!(numbers.len(), board.rows[row_number].len());
             for i in 0..(numbers.len()) {
-                board.rows[row_number][i].value=numbers.get(i).unwrap().clone();
+                board.rows[row_number][i].value = numbers.get(i).unwrap().clone();
             }
-            row_number= row_number + 1;
+            row_number = row_number + 1;
         }
     }
     boards.push(board);
     return boards;
 }
 
-fn play_game(bingo_numbers: Vec<i32>,boards: Vec<BingoBoard>) -> (i32, i32){
-    let mut myboards=boards.clone();
+fn play_game(bingo_numbers: Vec<i32>, boards: Vec<BingoBoard>) -> (i32, i32) {
+    let mut myboards = boards.clone();
     for number in bingo_numbers {
         for board in myboards.iter_mut() {
             board.mark_value(number);
             if board.is_finished() {
                 //println!("{}",board);
-                return (number,board.board_score());
+                return (number, board.board_score());
             }
         }
-
     }
     // Better errorcode
     panic!("Should not get here");
     // return (0,BingoBoard::new());
 }
 
+fn get_result_1(file: &str) -> i32 {
+    let input = utils::read_file(file);
+    let line = input.first().unwrap();
+    let bingo_numbers = read_bingo_numbers(line);
+    let boards = setup_boards(input);
+
+    let (number, board) = play_game(bingo_numbers, boards);
+    let board_score = board;
+    let game_score = board_score * number;
+    println!("Finalscore {} * {} = {}", number, board_score, game_score);
+
+    return game_score;
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{utils};
-    use crate::day4::{BingoBoard, Cell, play_game, read_bingo_numbers, setup_boards};
+    use crate::day4::{BingoBoard, Cell};
+    use crate::{day4, utils};
 
     #[test]
     fn part1() {
         println!("--- DAY 4-1 ----");
         println!("Lets Play Bingo!");
-        let _input = utils::read_file("inputs/4.txt");
-        let line = _input.first().unwrap();
-        let bingo_numbers = read_bingo_numbers(line);
-        let boards = setup_boards(_input);
-        let (number,board)=play_game(bingo_numbers,boards);
-        let board_score=board;
-        let game_score=board_score*number;
-        println!("Finalscore {} * {} = {}",number,board_score,game_score);
+        let game_score = day4::get_result_1("inputs/4_test.txt");
+        assert_eq!(game_score, 4512);
 
+        let game_score = day4::get_result_1("inputs/4.txt");
         // Wronganswers
-        assert_ne!(game_score,134505);
+        assert_ne!(game_score, 134505);
         // To low
-        //assert_ne!(game_score,42630);
-
-
-
+        assert_ne!(game_score, 42630);
     }
 
     #[test]
@@ -191,15 +199,56 @@ mod tests {
 
     #[test]
     fn test_finished() {
-
         let no_match = BingoBoard::new();
         assert!(!no_match.is_finished());
-        let horizontal = BingoBoard{
-            rows: [[Cell{value:0,marked:false};5],[Cell{value:0,marked:false};5],[Cell{value:0,marked:true};5],[Cell{value:0,marked:false};5],[Cell{value:0,marked:false};5]]
+        let horizontal = BingoBoard {
+            rows: [
+                [Cell {
+                    value: 0,
+                    marked: false,
+                }; 5],
+                [Cell {
+                    value: 0,
+                    marked: false,
+                }; 5],
+                [Cell {
+                    value: 0,
+                    marked: true,
+                }; 5],
+                [Cell {
+                    value: 0,
+                    marked: false,
+                }; 5],
+                [Cell {
+                    value: 0,
+                    marked: false,
+                }; 5],
+            ],
         };
         assert!(horizontal.is_finished());
-        let vertical = BingoBoard{
-            rows: [[Cell{value:0,marked:false},Cell{value:1,marked:true},Cell{value:0,marked:false},Cell{value:0,marked:false},Cell{value:0,marked:false}];5]
+        let vertical = BingoBoard {
+            rows: [[
+                Cell {
+                    value: 0,
+                    marked: false,
+                },
+                Cell {
+                    value: 1,
+                    marked: true,
+                },
+                Cell {
+                    value: 0,
+                    marked: false,
+                },
+                Cell {
+                    value: 0,
+                    marked: false,
+                },
+                Cell {
+                    value: 0,
+                    marked: false,
+                },
+            ]; 5],
         };
         assert!(vertical.is_finished());
 
@@ -208,7 +257,6 @@ mod tests {
 
     #[test]
     fn test_marking() {
-
         let mut board = BingoBoard::new();
         let marked = board.mark_value(404);
         assert!(marked);
@@ -219,13 +267,34 @@ mod tests {
     }
     #[test]
     fn test_boardscore() {
-
         let board = BingoBoard::new();
         let score = board.board_score();
-        assert_eq!(score, 25*404);
-        let score = BingoBoard{
-            rows: [[Cell{value:1,marked:false},Cell{value:1,marked:true},Cell{value:2,marked:false},Cell{value:3,marked:false},Cell{value:4,marked:false}];5]
-        }.board_score();
+        assert_eq!(score, 25 * 404);
+        let score = BingoBoard {
+            rows: [[
+                Cell {
+                    value: 1,
+                    marked: false,
+                },
+                Cell {
+                    value: 1,
+                    marked: true,
+                },
+                Cell {
+                    value: 2,
+                    marked: false,
+                },
+                Cell {
+                    value: 3,
+                    marked: false,
+                },
+                Cell {
+                    value: 4,
+                    marked: false,
+                },
+            ]; 5],
+        }
+        .board_score();
         assert_eq!(score, 50);
 
         return;
--- src/day5.rs
+++ src/day5.rs
@@ -1,138 +1,75 @@
+use crate::utils;
 
-use crate::{utils};
-
-#[derive(Copy, Clone,Debug)]
-struct Point{
+#[derive(Copy, Clone, Debug)]
+struct Point {
     x: i32,
     y: i32,
 }
 
-#[derive(Copy, Clone,Debug)]
-struct Line{
+#[derive(Copy, Clone, Debug)]
+struct Line {
     a: Point,
     b: Point,
 }
 
-
 impl Line {
-
-    fn is_horizontal (&self) -> bool{
+    fn is_horizontal(&self) -> bool {
         self.a.y == self.b.y
     }
-    fn is_vertical (&self) -> bool{
+    fn is_vertical(&self) -> bool {
         self.a.x == self.b.x
     }
 
-    fn points (&self) -> Vec<Point>{
+    fn points(&self) -> Vec<Point> {
         let mut points = Vec::new();
-
-
-        if self.a.x <= self.b.x {
-            for x in self.a.x..=self.b.x {
-                if self.a.y <= self.b.y {
-                    for y in self.a.y..=self.b.y {
-                        points.push(Point{
-                            x,y
-                        });
-                    }
-                } else {
-                    for y in self.b.y..=self.a.y {
-                        points.push(Point{
-                            x,y
-                        });
-                    }
-                }
-            }
-        } else {
-            for x in self.b.x..=self.a.x {
-                if self.a.y <= self.b.y {
-                    for y in self.a.y..=self.b.y {
-                        points.push(Point{
-                            x,y
-                        });
-                    }
-                } else {
-                    for y in self.b.y..=self.a.y {
-                        points.push(Point{
-                            x,y
-                        });
-                    }
-                }
-            }
-        }
-
-        assert!(!points.is_empty());
-        return points
-    }
-
-    fn second_points (&self) -> Vec<Point>{
-        let mut points = Vec::new();
-        if self.is_vertical(){
+        if self.is_vertical() {
             let x = self.a.x;
             if self.a.y <= self.b.y {
                 for y in self.a.y..=self.b.y {
-                    points.push(Point{
-                        x,y
-                    });
+                    points.push(Point { x, y });
                 }
             } else {
                 for y in self.b.y..=self.a.y {
-                    points.push(Point{
-                        x,y
-                    });
+                    points.push(Point { x, y });
                 }
             }
         } else if self.is_horizontal() {
             let y = self.a.y;
             if self.a.x <= self.b.x {
                 for x in self.a.x..=self.b.x {
-                    points.push(Point{
-                        x,y
-                    });
+                    points.push(Point { x, y });
                 }
             } else {
                 for x in self.b.x..=self.a.x {
-                    points.push(Point{
-                        x,y
-                    });
+                    points.push(Point { x, y });
                 }
             }
-        } else { //just diagonal
+        } else {
+            //just diagonal
             let mut x = self.a.x;
             let mut y = self.a.y;
             while x != self.b.x && y != self.b.y {
-                points.push(Point{
-                    x,y
-                });
+                points.push(Point { x, y });
                 if x < self.b.x {
-                    x += 1 ;
+                    x += 1;
                 }
                 if x > self.b.x {
-                    x -= 1 ;
+                    x -= 1;
                 }
                 if y < self.b.y {
-                    y += 1 ;
+                    y += 1;
                 }
                 if y > self.b.y {
-                    y -= 1 ;
+                    y -= 1;
                 }
             }
-            points.push(Point{
-                x,y
-            });
+            points.push(Point { x, y });
         }
 
-
-
-
         assert!(!points.is_empty());
-        return points
+        return points;
     }
-
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -143,36 +80,37 @@ mod tests {
     #[test]
     fn part1() {
         println!("--- DAY 5-1 ----");
-        let result_test=get_result_1("inputs/5_test.txt");
-        assert_eq!(result_test,5);
-        let result=get_result_1("inputs/5.txt");
+        let result_test = get_result_1("inputs/5_test.txt");
+        assert_eq!(result_test, 5);
+        let result = get_result_1("inputs/5.txt");
         println!("Hot Spots :{} ", result);
-        assert_eq!(result,6856)
+        assert_eq!(result, 6856)
     }
 
     #[test]
     fn part2() {
         println!("--- DAY 5-2 ----");
-        let result_test=get_result_2("inputs/5_test.txt");
-        assert_eq!(result_test,12);
-        let result=get_result_2("inputs/5.txt");
+        let result_test = get_result_2("inputs/5_test.txt");
+        assert_eq!(result_test, 12);
+        let result = get_result_2("inputs/5.txt");
         println!("Hot Spots :{} ", result);
-        assert_eq!(result,20666)
+        assert_eq!(result, 20666)
     }
 }
 
-fn get_result_1 (file: &str) -> usize{
+fn get_result_1(file: &str) -> usize {
     let input = utils::read_file(file);
     let lines: Vec<Line> = read_lines(input);
 
-    let filtered_lines = lines.iter()
-        .filter(|s|s.is_horizontal() || s.is_vertical())
-        .map(|s|s.to_owned())
+    let filtered_lines = lines
+        .iter()
+        .filter(|s| s.is_horizontal() || s.is_vertical())
+        .map(|s| s.to_owned())
         .collect::<Vec<Line>>();
 
     let map = draw_lines(filtered_lines);
     let mut values = Vec::new();
-    for row in map.iter(){
+    for row in map.iter() {
         for cell in row.iter() {
             if cell.to_owned() >= 2 {
                 values.push(cell);
@@ -180,17 +118,16 @@ fn get_result_1 (file: &str) -> usize{
         }
     }
 
-
     return values.len();
 }
 
-fn get_result_2 (file: &str) -> usize{
+fn get_result_2(file: &str) -> usize {
     let input = utils::read_file(file);
     let lines: Vec<Line> = read_lines(input);
 
     let map = draw_lines(lines);
     let mut values = Vec::new();
-    for row in map.iter(){
+    for row in map.iter() {
         for cell in row.iter() {
             if cell.to_owned() >= 2 {
                 values.push(cell);
@@ -198,54 +135,56 @@ fn get_result_2 (file: &str) -> usize{
         }
     }
 
-
     return values.len();
 }
 
-fn _print_map (map: Vec<Vec<i32>>) {
-    for row in map.iter(){
+fn _print_map(map: Vec<Vec<i32>>) {
+    for row in map.iter() {
         println!();
         for cell in row.iter() {
-            print!("{}",cell);
+            print!("{}", cell);
         }
     }
     println!();
 }
 
-fn read_lines (input: Vec<String>) -> Vec<Line>{
+fn read_lines(input: Vec<String>) -> Vec<Line> {
     let mut lines = Vec::new();
-    for input_line in input.iter(){
-        let raw_inputs=input_line
+    for input_line in input.iter() {
+        let raw_inputs = input_line
             .split(" -> ")
-            .map(|s|s.to_string())
+            .map(|s| s.to_string())
             .collect::<Vec<String>>();
-        let point_a = raw_inputs.get(0).unwrap()
+        let point_a = raw_inputs
+            .get(0)
+            .unwrap()
             .split(",")
-            .map(|s|s.to_string())
-            .map(|s|s.parse::<i32>().unwrap())
+            .map(|s| s.to_string())
+            .map(|s| s.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
-        let point_b = raw_inputs.get(1).unwrap()
+        let point_b = raw_inputs
+            .get(1)
+            .unwrap()
             .split(",")
-            .map(|s|s.to_string())
-            .map(|s|s.parse::<i32>().unwrap())
+            .map(|s| s.to_string())
+            .map(|s| s.parse::<i32>().unwrap())
             .collect::<Vec<i32>>();
-        let line = Line{
-            a: Point{
+        let line = Line {
+            a: Point {
                 x: point_a.get(0).unwrap().to_owned(),
                 y: point_a.get(1).unwrap().to_owned(),
             },
-            b: Point{
+            b: Point {
                 x: point_b.get(0).unwrap().to_owned(),
                 y: point_b.get(1).unwrap().to_owned(),
-            }
+            },
         };
         lines.push(line);
-
     }
     return lines;
 }
 
-fn draw_lines (lines: Vec<Line>) -> Vec<Vec<i32>>{
+fn draw_lines(lines: Vec<Line>) -> Vec<Vec<i32>> {
     let mut map = Vec::new();
     for _i in 0..1000 {
         let mut line = Vec::new();
@@ -254,11 +193,13 @@ fn draw_lines (lines: Vec<Line>) -> Vec<Vec<i32>>{
         }
         map.push(line);
     }
-    for line in lines{
-        for point in line.second_points() {
+    for line in lines {
+        for point in line.points() {
             let current = map
-                .get_mut(point.y as usize).unwrap()
-                .get_mut(point.x as usize).unwrap();
+                .get_mut(point.y as usize)
+                .unwrap()
+                .get_mut(point.x as usize)
+                .unwrap();
             *current += 1;
         }
     }
