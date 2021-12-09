e57ae3c9ee0392482bf5efb1dfb8dc85c4a1ddfd
Manuel Müller <manuel.mueller@geekinbusiness.de>
2021-12-09 13:18:33 +0100

    Day 9-1

--- src/day9.rs
+++ src/day9.rs
@@ -4,9 +4,57 @@ use crate::utils;
 
 
 pub fn get_result_1(file: &str) -> u64 {
-    let mut _heigtmap = read_file(file);
+    let heigtmap = read_file(file);
+    let mut low_points = Vec::new();
+    for x in 0..heigtmap.len() {
+        let row = heigtmap.get(x).unwrap();
+        for y in 0..row.len() {
+            match is_low_point(&heigtmap,x,y){
+                Some(v) => low_points.push(v),
+                _ => {},
+            };
+        }
+    }
+    let threath_level = low_points.iter()
+        .map(|x|x+1)
+        .map(|x|x as u64)
+        .reduce(|a,b|a+b)
+        .unwrap();
+    return threath_level as u64;
+}
 
-    return 0;
+fn is_low_point (heightmap: &Vec<Vec<u8>>,x:usize,y:usize) -> Option<u8> {
+    let row=heightmap.get(x).unwrap();
+    let point_val = *row.get(y).unwrap();
+    if x>0 {
+        let up = (x-1,y);
+        let up_val= *heightmap.get(up.0).unwrap().get(up.1).unwrap();
+        if up_val <= point_val {
+            return Option::None;
+        }
+    }
+    if x < heightmap.len()-1 {
+        let down = (x+1,y);
+        let down_val=*heightmap.get(down.0).unwrap().get(down.1).unwrap();
+        if down_val <= point_val {
+            return Option::None;
+        }
+    }
+    if y>0 {
+        let left = (x,y-1);
+        let left_val= *heightmap.get(left.0).unwrap().get(left.1).unwrap();
+        if left_val <= point_val {
+            return Option::None;
+        }
+    }
+    if y < row.len()-1 {
+        let right = (x,y+1);
+        let right_val=*heightmap.get(right.0).unwrap().get(right.1).unwrap();
+        if right_val <= point_val {
+            return Option::None;
+        }
+    }
+    return Option::Some(point_val);
 }
 
 
@@ -40,7 +88,7 @@ mod tests {
         let result = day9::get_result_1("inputs/9.txt");
 
         println!("Risklevel: {}",result);
-        assert_eq!(result, 355);
+        assert_eq!(result, 516);
     }
 
 
